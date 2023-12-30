09f30333860ea68de1f38178a64368d07f72c449
Marco Torres <marcoantonio.torres@icloud.com>
2023-12-30 10:57:45 -0500

    Created two additional components. The first is into-pdf (Planning to use this to include the code to turn the data into pdf). The second one is user-input (This will be used to let the user interact with the view and place data intoservie)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserInputComponent } from './user-input/user-input.component';
+import { IntoPdfComponent } from './into-pdf/into-pdf.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserInputComponent,
+    IntoPdfComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/into-pdf/into-pdf.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-into-pdf',
+  templateUrl: './into-pdf.component.html',
+  styleUrls: ['./into-pdf.component.css']
+})
+export class IntoPdfComponent {
+
+}
